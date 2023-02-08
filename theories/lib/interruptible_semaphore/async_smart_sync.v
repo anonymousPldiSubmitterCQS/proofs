7e22be54b6116e196102d920357ec09fed07142e
Anonymous Submitter <email.redacted.out@example.com>
2023-02-08 13:51:53 +0100

    Add an example client for a mutex

--- theories/lib/interruptible_semaphore/async_smart_sync.v
+++ theories/lib/interruptible_semaphore/async_smart_sync.v
@@ -259,3 +259,269 @@ Proof.
 Qed.
 
 End proof.
+
+Section client.
+
+Variable array_interface: infiniteArrayInterface.
+
+(* a workaround: this version of Iris doesn't support later credits, and we need
+   'awaitThreadQueueFuture' to make at least one execution step. *)
+Definition awaitThreadQueueFuture': val :=
+  λ: "f", let: "x" := awaitThreadQueueFuture "f" in #();; "x".
+
+Definition useSemaphore: expr :=
+  let: "l" := ref #0 in
+  let: "s" := newSemaphore array_interface #(1%nat) in
+  let: "doneMarker" := ref #false in
+  Fork (
+      awaitThreadQueueFuture' (acquireSemaphore array_interface "s") ;;
+      "l" <- !"l" + #1 ;;
+      releaseSemaphore array_interface "s" ;;
+      "doneMarker" <- #true
+  ) ;;
+  awaitThreadQueueFuture' (acquireSemaphore array_interface "s") ;;
+  "l" <- !"l" + #1 ;;
+  releaseSemaphore array_interface "s" ;;
+  (rec: "wait" "_" := (!"doneMarker" = #true) || "wait" #()) #() ;;
+  !"l".
+
+Notation threadState := (optionUR (csumR (exclR unitO) (agreeR unitO))).
+
+Notation clientStateR := (authUR (prodUR threadState threadState)).
+
+Class clientStateG Σ := ClientStateG {clientState_inG :> inG Σ clientStateR;}.
+Definition clientStateΣ : gFunctors := #[GFunctor clientStateR].
+Global Instance subG_clientStateR {Σ} : subG clientStateΣ Σ → clientStateG Σ.
+Proof. solve_inG. Qed.
+
+Context `{heapG Σ} `{iteratorG Σ} `{threadQueueG Σ} `{futureG Σ} `{clientStateG Σ}.
+Variable (N NFuture: namespace).
+Variable (HNDisj: N ## NFuture).
+Notation iProp := (iProp Σ).
+
+Variable array_spec: infiniteArraySpec _ array_interface.
+
+Definition weFinished γ := own γ (◯ (Some (Cinr (to_agree ())), None)).
+Definition parallelFinished γ := own γ (◯ (None, Some (Cinr (to_agree ())))).
+Definition ourToken γ := own γ (◯ (Some (Cinl (Excl ())), None)).
+Definition parallelToken γ := own γ (◯ (None, Some (Cinl (Excl ())))).
+
+Definition useSemaphore_inv_l γ l: iProp :=
+  (
+    l ↦{1/2} #2 ∗ own γ (● (Some (Cinr (to_agree ())), Some (Cinr (to_agree ())))) ∨
+    l ↦{1/2} #1 ∗ own γ (● (Some (Cinr (to_agree ())), Some (Cinl (Excl ())))) ∨
+    l ↦{1/2} #1 ∗ own γ (● (Some (Cinl (Excl ())), Some (Cinr (to_agree ())))) ∨
+    l ↦{1/2} #0 ∗ own γ (● (Some (Cinl (Excl ())), Some (Cinl (Excl ()))))
+  ).
+
+Definition useSemaphore_inv_doneMarker γ doneMarker: iProp :=
+  doneMarker ↦ #false ∨ doneMarker ↦ #true ∧ parallelFinished γ.
+
+Theorem awaitSemaphoreFuture R γa γtq γe γd s γf v:
+  {{{ is_semaphore N NFuture array_interface array_spec R γa γtq γe γd s
+      ∗ is_semaphore_future N NFuture array_interface array_spec R γtq γa γf v
+      ∗ thread_queue_future_cancellation_permit γf
+  }}}
+    awaitThreadQueueFuture' v
+  {{{ RET (InjRV #()); R }}}.
+Proof.
+  iIntros (Φ) "(#HIsSem & #HFuture & HCancPermit) HΦ".
+  iDestruct "HIsSem" as (b c d) "(H1 & H2 & HTq)". rewrite /awaitThreadQueueFuture'.
+  awp_apply await_thread_future without "HΦ"; [|done|done|].
+  by solve_ndisj.
+  iAaccIntro with "HCancPermit".
+  { iIntros ">H !>". by iFrame. }
+  simpl. iIntros (?) "(-> & R & _)". iModIntro. iIntros "HΦ".
+  wp_pures. iApply ("HΦ" with "R").
+Qed.
+
+Lemma finishLocalWork γ (e: threadState):
+  own γ (● (Some (Cinl (Excl ())), e)) -∗ own γ (◯ (Some (Cinl (Excl ())), None)) ==∗
+  own γ (● (Some (Cinr (to_agree ())), e)) ∗ weFinished γ.
+Proof.
+  iIntros "H1 H2". iMod (own_update_2 with "H1 H2") as "[$ $]"; last done.
+  apply auth_update, prod_local_update; last done; simpl.
+  etransitivity.
+  by apply delete_option_local_update, Cinl_exclusive, excl_exclusive.
+  by apply alloc_option_local_update.
+Qed.
+
+Lemma finishParallelWork γ (e: threadState):
+  own γ (● (e, Some (Cinl (Excl ())))) -∗ own γ (◯ (None, Some (Cinl (Excl ())))) ==∗
+  own γ (● (e, Some (Cinr (to_agree ())))) ∗ parallelFinished γ.
+Proof.
+  iIntros "H1 H2". iMod (own_update_2 with "H1 H2") as "[$ $]"; last done.
+  apply auth_update, prod_local_update; first done; simpl.
+  etransitivity.
+  by apply delete_option_local_update, Cinl_exclusive, excl_exclusive.
+  by apply alloc_option_local_update.
+Qed.
+
+Lemma CinlCinr_included {A B: cmraT} (a: A) (b: B): Some (Cinl a) ≼ Some (Cinr b) -> False.
+Proof.
+  intros contra. apply Some_included in contra. destruct contra as [contra|contra].
+  by inversion contra.
+  apply csum_included in contra.
+  destruct contra as [contra|[(? & ? & (_ & contra & _))|(? & ? & (contra & _ & _))]];
+    by inversion contra.
+Qed.
+
+Lemma CinrCinl_included {A: cmraT} {B: cmraT} (a: A) (b: B): Some (Cinr a) ≼ Some (Cinl b) -> False.
+Proof.
+  intros contra. apply Some_included in contra. destruct contra as [contra|contra].
+  by inversion contra.
+  apply csum_included in contra.
+  destruct contra as [contra|[(? & ? & (contra & _ & _))|(? & ? & (_ & contra & _))]];
+    by inversion contra.
+Qed.
+
+Theorem useSemaphore_proof:
+  {{{ inv_heap_inv }}} useSemaphore {{{ RET #2; True }}}.
+Proof.
+  iIntros (Φ) "HHeapInv HPost". rewrite /useSemaphore. wp_alloc l as "Hl". wp_pures.
+  wp_bind (newSemaphore _ _).
+  iMod (own_alloc (
+            ● (Some (Cinl (Excl ())), Some (Cinl (Excl ()))) ⋅
+            (◯ (None, Some (Cinl (Excl ()))) ⋅ ◯ (Some (Cinl (Excl ())), None))))
+    as (γ) "(H● & HParallel & HOur)"; first by apply auth_both_valid.
+  iDestruct "Hl" as "[Hl Hl']".
+  wp_apply (newSemaphore_spec N NFuture _ array_spec 1 (∃ n, l ↦{1/2} #n)%I with "[HHeapInv Hl']").
+  { iFrame "HHeapInv". simpl. iSplitL; last done. iExists 0. done. }
+  iIntros (γa γtq γe γd s) "#HIsSem". wp_pures.
+  wp_alloc doneMarker as "HDone".
+  iMod (inv_alloc N _ (useSemaphore_inv_l γ l) with "[H● Hl]") as "#HInv".
+  { iRight. iRight. iRight. iFrame. }
+  iMod (inv_alloc N _ (useSemaphore_inv_doneMarker γ doneMarker) with "[HDone]") as "#HInvDone";
+    first by iFrame.
+  wp_pures. wp_apply (wp_fork with "[HParallel]").
+  - iModIntro. wp_bind (acquireSemaphore _ _).
+    iApply (acquireSemaphore_spec with "[$]").
+    iIntros "!>" (γf v) "[#HFuture HCancPermit]".
+    wp_apply (awaitSemaphoreFuture with "[HCancPermit]").
+    by iFrame "HIsSem HFuture HCancPermit".
+    iIntros "HR". iDestruct "HR" as (n) "Hl". wp_pures.
+    wp_bind (!#l)%E.
+    iInv "HInv" as "[(_ & >H●)|[(>Hl' & >H●)|[(_ & >H●)|(>Hl' & >H●)]]]".
+    all: try by iDestruct (own_valid_2 with "H● HParallel") as
+          %[[_ Hr%CinlCinr_included]%pair_included _]%auth_both_valid.
+    * iDestruct (mapsto_agree with "Hl Hl'") as %->.
+      wp_load. iModIntro. iSplitL "H● Hl'".
+      { iRight. iLeft. by iFrame. }
+      wp_pures. wp_bind (#l <- _)%E.
+      iInv "HInv" as "[(_ & >H●)|[(>Hl' & >H●)|[(_ & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HParallel") as
+            %[[_ Hr%CinlCinr_included]%pair_included _]%auth_both_valid.
+      2: by iDestruct (mapsto_agree with "Hl Hl'") as %HEq.
+      iCombine "Hl" "Hl'" as "Hl".
+      wp_store.
+      iDestruct "Hl" as "[Hl Hl']".
+      iMod (finishParallelWork with "H● HParallel") as "[H● #HParallelFinished]".
+      iSplitL "Hl H●".
+      { iLeft. by iFrame. }
+      iModIntro. wp_pures.
+      wp_apply (releaseSemaphore_spec with "[Hl']").
+      2: by iFrame "HIsSem"; iExists 2; iFrame "Hl'".
+      1: by solve_ndisj.
+      iIntros "_". wp_pures.
+      iInv "HInvDone" as "[HDone|[HDone _]]"; wp_store; (iSplitL; [iRight; by iFrame|done]).
+    * iDestruct (mapsto_agree with "Hl Hl'") as %->.
+      wp_load. iModIntro. iSplitL "H● Hl'".
+      { iRight. iRight. iRight. by iFrame. }
+      wp_pures. wp_bind (#l <- _)%E.
+      iInv "HInv" as "[(_ & >H●)|[(>Hl' & >H●)|[(_ & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HParallel") as
+            %[[_ Hr%CinlCinr_included]%pair_included _]%auth_both_valid.
+      1: by iDestruct (mapsto_agree with "Hl Hl'") as %HEq.
+      iCombine "Hl" "Hl'" as "Hl".
+      wp_store.
+      iDestruct "Hl" as "[Hl Hl']".
+      iMod (finishParallelWork with "H● HParallel") as "[H● #HParallelFinished]".
+      iSplitL "Hl H●".
+      { iRight. iRight. iLeft. by iFrame. }
+      iModIntro. wp_pures.
+      wp_apply (releaseSemaphore_spec with "[Hl']").
+      2: by iFrame "HIsSem"; iExists 1; iFrame "Hl'".
+      1: by solve_ndisj.
+      iIntros "_". wp_pures.
+      iInv "HInvDone" as "[HDone|[HDone _]]"; wp_store; (iSplitL; [iRight; by iFrame|done]).
+  - wp_pures. wp_bind (acquireSemaphore _ _).
+    iApply (acquireSemaphore_spec with "[$]").
+    iIntros "!>" (γf v) "[#HFuture HCancPermit]".
+    wp_apply (awaitSemaphoreFuture with "[HCancPermit]").
+    by iFrame "HIsSem HFuture HCancPermit".
+    iIntros "HR". iDestruct "HR" as (n) "Hl". wp_pures.
+    wp_bind (!#l)%E.
+    iInv "HInv" as "[(>Hl' & >H●)|[(>Hl' & >H●)|[(>Hl' & >H●)|(>Hl' & >H●)]]]".
+    all: try by iDestruct (own_valid_2 with "H● HOur") as
+          %[[Hr%CinlCinr_included _]%pair_included _]%auth_both_valid.
+    * iDestruct (mapsto_agree with "Hl Hl'") as %->.
+      wp_load. iModIntro. iSplitL "H● Hl'".
+      { iRight. iRight. iLeft. by iFrame. }
+      wp_pures. wp_bind (#l <- _)%E.
+      iInv "HInv" as "[(>Hl' & >H●)|[(>Hl' & >H●)|[(>Hl' & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HOur") as
+            %[[Hr%CinlCinr_included _]%pair_included _]%auth_both_valid.
+      2: by iDestruct (mapsto_agree with "Hl Hl'") as %HEq.
+      iCombine "Hl" "Hl'" as "Hl".
+      wp_store.
+      iDestruct "Hl" as "[Hl Hl']".
+      iMod (finishLocalWork with "H● HOur") as "[H● #HWorkFinished]".
+      iSplitL "Hl H●".
+      { iLeft. by iFrame. }
+      iModIntro. wp_pures.
+      wp_apply (releaseSemaphore_spec with "[Hl']").
+      2: by iFrame "HIsSem"; iExists 2; iFrame "Hl'".
+      1: by solve_ndisj.
+      iIntros "_". wp_pures.
+      iLöb as "IH". wp_bind (!#doneMarker)%E.
+      iInv "HInvDone" as "[HDone|[HDone #HTheyFinished]]" "HClose".
+      { wp_load. iMod ("HClose" with "[$]") as "_". iModIntro.
+        wp_pures. iApply ("IH" with "HPost"). }
+      wp_load. iMod ("HClose" with "[HDone]") as "_"; first by iRight; iFrame.
+      iModIntro. wp_pures. iClear "IH".
+      iInv "HInv" as "[(>Hl' & >H●)|[(>Hl' & >H●)|[(>Hl' & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HWorkFinished") as
+            %[[Hr%CinrCinl_included _]%pair_included _]%auth_both_valid.
+      all: try by iDestruct (own_valid_2 with "H● HTheyFinished") as
+            %[[_ Hr%CinrCinl_included]%pair_included _]%auth_both_valid.
+      wp_load.
+      iSplitR "HPost".
+      { iLeft. iFrame. done. }
+      by iApply "HPost".
+    * iDestruct (mapsto_agree with "Hl Hl'") as %->.
+      wp_load. iModIntro. iSplitL "H● Hl'".
+      { iRight. iRight. iRight. by iFrame. }
+      wp_pures. wp_bind (#l <- _)%E.
+      iInv "HInv" as "[(>Hl' & >H●)|[(>Hl' & >H●)|[(>Hl' & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HOur") as
+            %[[Hr%CinlCinr_included _]%pair_included _]%auth_both_valid.
+      1: by iDestruct (mapsto_agree with "Hl Hl'") as %HEq.
+      iCombine "Hl" "Hl'" as "Hl".
+      wp_store.
+      iDestruct "Hl" as "[Hl Hl']".
+      iMod (finishLocalWork with "H● HOur") as "[H● #HWorkFinished]".
+      iSplitL "Hl H●".
+      { iRight. iLeft. by iFrame. }
+      iModIntro. wp_pures.
+      wp_apply (releaseSemaphore_spec with "[Hl']").
+      2: by iFrame "HIsSem"; iExists 1; iFrame "Hl'".
+      1: by solve_ndisj.
+      iIntros "_". wp_pures.
+      iLöb as "IH". wp_bind (!#doneMarker)%E.
+      iInv "HInvDone" as "[HDone|[HDone #HTheyFinished]]" "HClose".
+      { wp_load. iMod ("HClose" with "[$]") as "_". iModIntro.
+        wp_pures. iApply ("IH" with "HPost"). }
+      wp_load. iMod ("HClose" with "[HDone]") as "_"; first by iRight; iFrame.
+      iModIntro. wp_pures. iClear "IH".
+      iInv "HInv" as "[(>Hl' & >H●)|[(>Hl' & >H●)|[(>Hl' & >H●)|(>Hl' & >H●)]]]".
+      all: try by iDestruct (own_valid_2 with "H● HWorkFinished") as
+            %[[Hr%CinrCinl_included _]%pair_included _]%auth_both_valid.
+      all: try by iDestruct (own_valid_2 with "H● HTheyFinished") as
+            %[[_ Hr%CinrCinl_included]%pair_included _]%auth_both_valid.
+      wp_load.
+      iSplitR "HPost".
+      { iLeft. iFrame. done. }
+      by iApply "HPost".
+Qed.
+
+End client.
